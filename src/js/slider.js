537edb298c1ce8c12a832adcd4600de516ba3def
fedbka <fedbka@yandex.com>
2022-09-11 16:33:07 +0400

    fix: resolve some artefacts on mobile layouts

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,13 @@
 function makeSlide(index, allSlides, allButtons) {
 
-    toogleButtonCssClassName = 'slider__button_type_current';
     const button = allButtons[index];
     const slide = allSlides[index];
+    
+    toogleButtonCssClassName = 'slider__button_type_current';
     toggleItemCssClassName = 'slider__item_type_hidden';
 
-    if (slide.classList.contains('images__item')) toggleItemCssClassName = 'slider__item_type_hidden-image';
-    if (slide.classList.contains('cards__item')) toggleItemCssClassName = 'slider__item_type_hidden-card';
+    if (slide.classList.contains('images__item')) toggleItemCssClassName = toggleItemCssClassName + '-image';
+    if (slide.classList.contains('cards__item')) toggleItemCssClassName = toggleItemCssClassName + '-card';
 
     allButtons.forEach(item => item == button ? item.classList.add(toogleButtonCssClassName) : item.classList.remove(toogleButtonCssClassName));
     allSlides.forEach(item => item == slide ? item.classList.remove(toggleItemCssClassName) : item.classList.add(toggleItemCssClassName))
@@ -15,13 +16,13 @@ function makeSlide(index, allSlides, allButtons) {
 
 function addSliderFunctionality(section) {
 
-    const sliders = [...section.querySelectorAll('.slider__items')];
-    const slidersButtons = [...section.querySelectorAll('.slider__buttons')];
+    const allSliders = [...section.querySelectorAll('.slider__items')];
+    const allSlidersButtons = [...section.querySelectorAll('.slider__buttons')];
 
-    sliders.forEach((slider, index) => {
-        const sliderButtons = slidersButtons[index];
+    allSliders.forEach((slider, index) => {
+ 
         const slides = [...slider.querySelectorAll('.slider__item')];
-        const buttons = [...sliderButtons.querySelectorAll('.slider__button')];
+        const buttons = [...allSlidersButtons[index].querySelectorAll('.slider__button')];
 
         buttons.forEach((button, index, allButtons) => {
             button.addEventListener('click', (event) => makeSlide(index, slides, allButtons))
